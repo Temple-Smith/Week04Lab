588096e788c4febee639b07d8dee5e158be7e252
Drew- <Drew-@MSI>
2022-09-23 00:47:41 -0600

    Finished Week 4 Lab.

--- src/java/servlets/NoteServlet.java
+++ src/java/servlets/NoteServlet.java
@@ -1,6 +1,12 @@
 package servlets;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,45 +22,58 @@ public class NoteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        //declare variable of javabean Note class.
+        Note vNote;
+        //Following code is provided by instructor
+        //find real path for note.txt in user's environment
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
         
-        String edit = request.getParameter("edit");
-        request.setAttribute("edit",edit);
-        edit = "yes";
+        //Research BufferedReader again...
+        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+        //instantiate vnote variable
+        vNote = new Note(br.readLine(), br.readLine());
+        br.close();
         
+        request.setAttribute("title", vNote.getTitle());
+        request.setAttribute("contents", vNote.getContents());
         
-        if (edit.equals("no")) {
+       if(request.getParameter("edit") == null){
         getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp")
                 .forward(request, response);
-        edit = "yes";
-        }
-        
-
-        else if(edit.equals("yes")) {
+       }
+       else {
         getServletContext().getRequestDispatcher("/WEB-INF/editnote.jsp")
                 .forward(request, response);
-       }
-        return;
+        }
     }
 
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-  
         
-        String edit = request.getParameter("edit");
-        request.setAttribute("edit",edit);
-        edit = "yes";
-        if(edit.equals("yes")) {
+        //declare variable of javabean Note class.
+        Note eNote;
+        //Following code is provided by instructor
+        //find real path for note.txt in user's environment
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+        
+        //Research BufferedReader again...
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path, false)));
+        //instantiate vnote variable
+        eNote = new Note(request.getParameter("title"), request.getParameter("contents"));
+     
+        
+        pw.println(eNote.getTitle());
+        pw.println(eNote.getContents());
+        pw.close();
+        
+        request.setAttribute("title", eNote.getTitle());
+        request.setAttribute("contents", eNote.getContents()); 
+        
+        
         getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp")
                 .forward(request, response);
-       }
-        return;
-//        getServletContext().getRequestDispatcher("/WEB-INF/editnote.jsp")
-//                .forward(request, response);
-//
-//        return;
     }
 }
 
